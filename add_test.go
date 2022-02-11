75c52349002787f38f53eb99b4af474e8ff03216
James <jismith1989@googlemail.com>
2022-02-11 10:27:18 +0000

    Improve test for Add function

--- add_test.go
+++ add_test.go
@@ -30,10 +30,11 @@ func TestAdd(t *testing.T) {
 		dataMutex.RLock()
 
 		assert.Equal(t, expectedLen, len(data))
+		assert.Equal(t, "1", data[0].bitmaskValue.String())
 	}()
 
 	Add(AddOptions{
-		Name:    "test",
+		Name:    "test2",
 		Handler: handler,
 	})
 
@@ -42,6 +43,7 @@ func TestAdd(t *testing.T) {
 		dataMutex.RLock()
 
 		assert.Equal(t, expectedLen, len(data))
+		assert.Equal(t, "11", data[0].bitmaskValue.String())
 	}()
 
 	handler = func() {}
@@ -58,5 +60,7 @@ func TestAdd(t *testing.T) {
 		dataMutex.RLock()
 
 		assert.Equal(t, expectedLen, len(data))
+		assert.Equal(t, "11", data[0].bitmaskValue.String())
+		assert.Equal(t, "1", data[1].bitmaskValue.String())
 	}()
 }
