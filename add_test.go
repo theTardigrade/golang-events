a5788051f71014b7ea7ffd67901611cd28c79b70
James <jismith1989@googlemail.com>
2022-02-11 10:33:12 +0000

    Simplify test for Add function

--- add_test.go
+++ add_test.go
@@ -7,13 +7,11 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	var expectedLen int
-
 	func() {
 		defer dataMutex.RUnlock()
 		dataMutex.RLock()
 
-		assert.Equal(t, expectedLen, len(data))
+		assert.Equal(t, 0, len(data))
 	}()
 
 	handler := func() {}
@@ -23,13 +21,11 @@ func TestAdd(t *testing.T) {
 		Handler: handler,
 	})
 
-	expectedLen++
-
 	func() {
 		defer dataMutex.RUnlock()
 		dataMutex.RLock()
 
-		assert.Equal(t, expectedLen, len(data))
+		assert.Equal(t, 1, len(data))
 		assert.Equal(t, "1", data[0].bitmaskValue.String())
 		assert.Equal(t, false, data[0].shouldWaitTillDone)
 		assert.Equal(t, HandlerOrder(0), data[0].order)
@@ -47,7 +43,7 @@ func TestAdd(t *testing.T) {
 		defer dataMutex.RUnlock()
 		dataMutex.RLock()
 
-		assert.Equal(t, expectedLen, len(data))
+		assert.Equal(t, 1, len(data))
 		assert.Equal(t, "11", data[0].bitmaskValue.String())
 	}()
 
@@ -58,13 +54,11 @@ func TestAdd(t *testing.T) {
 		Handler: handler,
 	})
 
-	expectedLen++
-
 	func() {
 		defer dataMutex.RUnlock()
 		dataMutex.RLock()
 
-		assert.Equal(t, expectedLen, len(data))
+		assert.Equal(t, 2, len(data))
 		assert.Equal(t, "11", data[0].bitmaskValue.String())
 		assert.Equal(t, "1", data[1].bitmaskValue.String())
 	}()
